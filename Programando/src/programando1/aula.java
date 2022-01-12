1302c2599088bca1298783863a71ddf5984e2d04
kestn <kestn@DESKTOP-SRDCO2H>
2022-01-11 22:49:46 -0200

    media

--- Programando/src/programando1/aula.java
+++ Programando/src/programando1/aula.java
@@ -6,20 +6,19 @@ public class aula{
 	public static void main(String[] args) {
 	
 	
-	int mediaAluno = 70;
-	String nome = "Junior";
-	
-	
-	if (mediaAluno >= 70 && nome.equals("Alex")) {
-		System.out.println("Parabens voce esta aprovado");	
-	     }else if (mediaAluno < 70) {
-		System.out.println("voce foi reprovado");
-	     } else {
-	            	 System.out.println("Aluno não encontrado");
-	             
-	             }
-	
+	int nota1 = 90;
+	int nota2 = 60;
+	int nota3 = 70;
+	int nota4 = 80;
+	int media = 0;
 
-	
+	media =(nota1 + nota2 + nota3 + nota4) / 4;
+		
+		if (media >=70) {
+			System.out.println("Aluno aprovado: " + media );
+			
+		} else {
+			System.out.println("Aluno reprovado:" + media);
+		}
 	}
 }
