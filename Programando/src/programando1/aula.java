5f3b0dd5763157173e83294360b5524c1b60ce44
kestn <kestn@DESKTOP-SRDCO2H>
2022-01-11 22:55:33 -0200

    if else em medias

--- Programando/src/programando1/aula.java
+++ Programando/src/programando1/aula.java
@@ -1,26 +1,24 @@
 package programando1;
 
-public class aula{ 
-	
+public class aula {
+
 	/* main e um metodo auto executavel em Java */
 	public static void main(String[] args) {
-	
-	
-	int nota1 = 10;
-	int nota2 = 60;
-	int nota3 = 70;
-	int nota4 = 80;
-	int media = 0;
 
-	media =(nota1 + nota2 + nota3 + nota4) / 4;
-		
-		if (media >=70) {
-			System.out.println("Aluno aprovado: " + media );
-			
-		} if ( media >= 40 && media <= 69) {
-			System.out.println("Aluno em recupeção:" + media );
-		}
-		else {
+		int nota1 = 10;
+		int nota2 = 60;
+		int nota3 = 9;
+		int nota4 = 80;
+		int media = 0;
+
+		media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+		if (media >= 70) {
+			System.out.println("Aluno aprovado: " + media);
+
+		} else if (media >= 40 && media <= 69) {
+			System.out.println("Aluno em recupeção:" + media);
+		} else {
 			System.out.println("Aluno reprovado:" + media);
 		}
 	}
