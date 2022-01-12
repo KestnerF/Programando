befb9cd89d804d4780e1f92bc9a5ffd2949372f8
kestn <kestn@DESKTOP-SRDCO2H>
2022-01-11 22:42:56 -0200

    aprovado reprovado ou nao encontrado

--- Programando/src/programando1/aula.java
+++ Programando/src/programando1/aula.java
@@ -1,5 +1,25 @@
 package programando1;
 
-public class aula {
+public class aula{ 
+	
+	/* main e um metodo auto executavel em Java */
+	public static void main(String[] args) {
+	
+	
+	int mediaAluno = 70;
+	String nome = "Junior";
+	
+	
+	if (mediaAluno >= 70 && nome.equals("Alex")) {
+		System.out.println("Parabens voce esta aprovado");	
+	     }else if (mediaAluno < 70) {
+		System.out.println("voce foi reprovado");
+	     } else {
+	            	 System.out.println("Aluno não encontrado");
+	             
+	             }
+	
 
+	
+	}
 }
