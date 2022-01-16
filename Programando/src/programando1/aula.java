c413324845153692059b82a54bdb97104f1359ba
kestn <kestn@DESKTOP-SRDCO2H>
2022-01-16 18:26:34 -0200

    Modulo : Resto da Divisao %

--- Programando/src/programando1/aula.java
+++ Programando/src/programando1/aula.java
@@ -5,16 +5,17 @@ public class aula {
 	/* main e um metodo auto executavel em Java */
 	public static void main(String[] args) {
 
-		/* Estrutura de repeticao FOR e continue*/
-
-		for (int numero = 0; numero <= 10; numero ++) {
-			if (numero == 3 || numero == 6 || numero == 9) {
+		/* Modulo : Resto da Divisao % */
+		
+		double carro = 9;
+		double pessoa = 2;
 				
-				System.out.println("obaa, encontrei o numero " + numero);
-				continue;
-			}
-			
-		}
+		double resto = carro % pessoa;
+		
+		System.out.println("Sobraram exatamente :" + resto + " carros.");
+		
+		
+		
 
 	}
 
