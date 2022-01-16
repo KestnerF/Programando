2773f37b465288e50582ab4fa420461df632eaee
kestn <kestn@DESKTOP-SRDCO2H>
2022-01-16 18:10:55 -0200

    Estrutura de repeticao FOR e continue

--- Programando/src/programando1/aula.java
+++ Programando/src/programando1/aula.java
@@ -5,14 +5,13 @@ public class aula {
 	/* main e um metodo auto executavel em Java */
 	public static void main(String[] args) {
 
-		/* Estrutura de repeticao FOR com break(parada) */
+		/* Estrutura de repeticao FOR e continue*/
 
 		for (int numero = 0; numero <= 10; numero ++) {
-			if (numero == 3) {
+			if (numero == 3 || numero == 6 || numero == 9) {
 				
-				System.out.println("obaa, encontrei o numero 3");
-				System.out.println("estou parando de executar");
-				break;
+				System.out.println("obaa, encontrei o numero " + numero);
+				continue;
 			}
 			
 		}
