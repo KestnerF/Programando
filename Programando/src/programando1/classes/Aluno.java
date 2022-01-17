e7a8494d93b77cc4b542f589c6dd6983ea25d725
kestn <kestn@DESKTOP-SRDCO2H>
2022-01-17 21:26:08 -0200

    adicionando classe

---
+++ Programando/src/programando1/classes/Aluno.java
@@ -0,0 +1,17 @@
+package programando1.classes;
+
+public class Aluno {
+
+	String nome;
+	int idade;
+	String  dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	
+	
+	
+	
+}
